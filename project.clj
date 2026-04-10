8b3f0e689b3c3e29168fec1ca84f553bde0c353d
mrrodriguez <mjr4184@gmail.com>
2026-04-10 12:21:54 -0500

    Setup project suitable for clojars deploy

--- project.clj
+++ project.clj
@@ -17,16 +17,16 @@
   :resource-paths ["clj-kondo"]
   :test-paths ["src/test/clojure"]
   :java-source-paths ["src/main/java"]
-  :javac-options ["-target" "1.8" "-source" "1.8"]
+  :javac-options ["--release" "11"]
   :clean-targets ^{:protect false} ["target"]
   :repl-options {:timeout 180000}
   :test-selectors {:default (complement (fn [x]
                                           (let [blacklisted-packages #{"generative" "performance"}
                                                 patterns (into []
-                                                           (comp
-                                                             (map #(str "^metasimple\\.concrete\\." % ".*"))
-                                                             (interpose "|"))
-                                                           blacklisted-packages)]
+                                                               (comp
+                                                                (map #(str "^metasimple\\.concrete\\." % ".*"))
+                                                                (interpose "|"))
+                                                               blacklisted-packages)]
                                             (some->> x :ns ns-name str (re-matches (re-pattern (apply str patterns)))))))
                    :generative (fn [x] (some->> x :ns ns-name str (re-matches #"^metasimple\.concrete\.generative.*")))
                    :performance (fn [x] (some->> x :ns ns-name str (re-matches #"^metasimple\.concrete\.performance.*")))}
@@ -36,8 +36,7 @@
                               [:id "mrrodriguez"]
                               [:name "Mike Rodriguez"]
                               [:url "https://metasimple.org"]]]
-  :deploy-repositories [["snapshots" {:url "https://oss.sonatype.org/content/repositories/snapshots/"
-                                      :creds :gpg}]
-                        ["releases" {:url "https://repo.clojars.org"
-                                     :creds :gpg
-                                     :sign-releases false}]])
+  :deploy-repositories [["releases" {:url "https://repo.clojars.org"
+                                     :username :env/CLOJARS_USERNAME
+                                     :password :env/CLOJARS_PASSWORD
+                                     :sign-releases true}]])
